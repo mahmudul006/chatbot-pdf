bcdc261a48537c18faf6af288b91bbb07debaff1
mahmudul006 <mahmudul.hasan@selisegroup.com>
2024-05-06 18:11:32 +0600

    minimal added

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Chat } from 'src/sections/one/chat';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Dashboard: One',
+};
+
+export default function Page() {
+  // return <OneView />;
+  return <Chat />;
+}
